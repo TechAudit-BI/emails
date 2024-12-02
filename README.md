c5bcf790a95a92a02de2a6e2af70ee7e8de6f452
Irina Shebarshina <76280175+rekilina@users.noreply.github.com>
2024-12-02 18:18:46 +0300

    email: license-purchase (#1)
    Co-authored-by: lidiya <shulepovalidiya@gmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+# Email Templates
+
+This repository contains email templates in MJML.
+
+### Scripts
+Detects if you misplaced or mispelled a MJML component, or 
+if you used any unauthorised attribute on a specific component. 
+```bash
+npm run validate
+```
+Writes the output to [output]:
+```bash
+npm run build:[output]
+```
+Watches the changes made to [input] (file or folder):
+```bash
+npm run watch:[input]
+```
---
+++ scripts/watch.js
@@ -0,0 +1,37 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const folder = process.argv[2];
+if (!folder) {
+	console.error('Specify a folder, e.g., npm run watch new-release');
+	process.exit(1);
+}
+
+if (!fs.existsSync(`templates/${folder}`)) {
+	console.error('Specify an existing folder');
+	process.exit(1);
+}
+
+const input = `templates/${folder}/index.mjml`;
+const outputDir = `dist/${folder}`;
+
+if (!fs.existsSync(outputDir)) {
+	// create directory if doesn't exist
+	fs.mkdirSync(outputDir, { recursive: true });
+}
+
+const command = `mjml -w ${input} -o ${outputDir}`;
+
+console.log(`Watching: ${folder}`);
+
+exec(command, (error, stdout, stderr) => {
+	if (error) {
+		console.error(`error: ${error.message}`);
+		return;
+	}
+	if (stderr) {
+		console.error(`stderr: ${stderr}`);
+		return;
+	}
+	console.log(`stdout: ${stdout}`);
+});
